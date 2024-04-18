bea0e13c65c7204b9465f5b09826bbe701186121
Eugene <e@egeek.me>
2024-04-18 08:39:55 +0300

    Download incidents and cameras files

--- az.hs
+++ az.hs
@@ -3,5 +3,18 @@
 
 {-# OPTIONS_GHC -Wall #-}
 
+import Conduit
+import Network.HTTP.Client
+import Network.HTTP.Simple
+
+type URL = String
+
+getFile :: URL -> FilePath -> IO ()
+getFile url file = do
+  req <- parseUrlThrow url
+  runResourceT $ httpSink req $ \_ -> sinkFile file
+
 main :: IO ()
-main = putStrLn "az"
+main = do
+  getFile "https://www.az511.gov/map/mapIcons/Incidents" "incidents.json"
+  getFile "https://www.az511.gov/map/mapIcons/Cameras" "cameras.json"
