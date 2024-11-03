ffa9677b5885462eb67394158ce32b185c5d705a
eugene <e@egeek.me>
2024-11-03 17:57:00 +0200

    Bump version to 0.5.0

--- az.hs
+++ az.hs
@@ -51,7 +51,7 @@ import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Match
 
 version :: Version
-version = makeVersion [0, 4, 2]
+version = makeVersion [0, 5, 0]
 
 userAgent :: ByteString
 userAgent = "az/" <> C8.pack (showVersion version)
